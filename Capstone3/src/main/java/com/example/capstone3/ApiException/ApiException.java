732d64bf9008db0a8a3c5dfe21678dc8b5d3fe0a
Bader Khaled <148685831+vk1ll3r@users.noreply.github.com>
2023-12-25 10:06:13 +0300

    Delete Capstone3 directory

--- Capstone3/src/main/java/com/example/capstone3/ApiException/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.capstone3.ApiException;
-
-public class ApiException extends RuntimeException {
-
-    public ApiException(String message){
-    super(message);
-    }
-}
--- Capstone3/src/main/java/com/example/capstone3/Repository/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.capstone3.Repository;
-
-import com.example.capstone3.Model.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice,Integer> {
-    Invoice findInvoiceById(Integer id);
-}
